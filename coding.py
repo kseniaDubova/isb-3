82b59dd7ef526af24d75f6c41aa21f4300ad275d
kseniaDubova <ksushadubova@icloud.com>
2023-05-02 11:14:14 +0400

    исправлены ошибки

--- coding.py
+++ coding.py
@@ -1,20 +1,21 @@
+import logging
 import os
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import padding
+
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import padding as sym_padding
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-import logging
-from PyQt5.QtWidgets import QFileDialog
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
+from cryptography.hazmat.primitives.ciphers import (Cipher, algorithms, modes)
 
 
-class Coding:
-    def __init__(self, size: int) -> None:
+class Encryption:
+    def __init__(self, size: int, way: str) -> None:
+        """
+        Функция инициализации
 
+        """
         self.size = int(size/8)
-        self.way = str(QFileDialog.getExistingDirectory(
-            caption='Выбор директории'))
+        self.way = way
         self.settings = {
             'encrypted_file': os.path.join(self.way, 'encrypted_file.txt'),
             'decrypted_file': os.path.join(self.way, 'decrypted_file.txt'),
@@ -25,14 +26,16 @@ class Coding:
         }
 
     def generation_key(self) -> None:
+        """
+        Функция генерации ключей
 
+        """
         keys = rsa.generate_private_key(
             public_exponent=65537,
             key_size=2048
         )
         private_key = keys
         public_key = keys.public_key()
-
         try:
             with open(self.settings['public_key'], 'wb') as public_out:
                 public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
@@ -40,7 +43,8 @@ class Coding:
         except OSError as err:
             logging.warning(
                 f"{err} ошибка при записи в файл {self.settings['public_key']}")
-
+        else:
+            logging.info("Публичный ключ записан")
         try:
             with open(self.settings['private_key'], 'wb') as private_out:
                 private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
@@ -49,18 +53,25 @@ class Coding:
         except OSError as err:
             logging.warning(
                 f"{err} ошибка при записи в файл {self.settings['private_key']}")
-
+        else:
+            logging.info("Приватный ключ записан")
         symmetric_key = os.urandom(self.size)
-        ciphertext = public_key.encrypt(symmetric_key, padding.OAEP(mgf=padding.MGF1(
-            algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+        ciphertext = public_key.encrypt(symmetric_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
         try:
             with open(self.settings['symmetric_key'], "wb") as f:
                 f.write(ciphertext)
         except OSError as err:
             logging.warning(
                 f"{err} ошибка при записи в файл {self.settings['symmetric_key']}")
+        else:
+            logging.info("Симметричный ключ записан")
 
     def __sym_key(self) -> bytes:
+        """
+        Функция расшифровки ключа симметричного шифрования
+
+        Возвращает расшифрованный симметричный ключ
+        """
         try:
             with open(self.settings['private_key'], "rb") as f:
                 private_key = serialization.load_pem_private_key(
@@ -68,7 +79,6 @@ class Coding:
         except OSError as err:
             logging.warning(
                 f"{err} ошибка при чтении из файла {self.settings['private_key']}")
-
         try:
             with open(self.settings['symmetric_key'], "rb") as f:
                 encrypted_symmetric_key = f.read()
@@ -77,42 +87,49 @@ class Coding:
         except OSError as err:
             logging.warning(
                 f"{err} ошибка при чтении из файла {self.settings['symmetric_key']}")
-
         return symmetric_key
 
-    def encryption(self) -> None:
-        way_e = str(QFileDialog.getOpenFileName(
-            caption='Выберите файл для шифрования', filter='*.txt'))
-        way_e = way_e.split('\'')[1]
+    def encryption(self, way: str) -> None:
+        """
+        Функция зашифровки текста алгоритмом 3DES
+
+        """
+        way_e = way
         symmetric_key = self.__sym_key()
         try:
             with open(way_e, 'r', encoding='utf-8') as f:
                 text = f.read()
         except OSError as err:
             logging.warning(f"{err} ошибка при чтении из файла {way_e}")
-
+        else:
+            logging.info("Текст прочитан")
         padder = sym_padding.ANSIX923(128).padder()
         padded_text = padder.update(bytes(text, 'utf-8')) + padder.finalize()
         iv = os.urandom(8)
         cipher = Cipher(algorithms.TripleDES(symmetric_key), modes.CBC(iv))
         encryptor = cipher.encryptor()
         c_text = encryptor.update(padded_text) + encryptor.finalize()
-
         try:
             with open(self.settings['iv_path'], 'wb') as key_file:
                 key_file.write(iv)
         except OSError as err:
             logging.warning(
                 f"{err} ошибка при записи в файл {self.settings['iv_path']}")
-
         try:
             with open(self.settings['encrypted_file'], 'wb') as f_text:
                 f_text.write(c_text)
         except OSError as err:
             logging.warning(
                 f"{err} ошибка при записи в файл {self.settings['encrypted_file']}")
+        else:
+            logging.info("Тескт зашифрован")
 
-    def decryption(self) -> None:
+    def decryption(self) -> str:
+        """
+        Функция расшифровки текста алгоритма 3DES
+
+        Возвращает путь до расщифрованного файла
+        """
         symmetric_key = self.__sym_key()
         try:
             with open(self.settings['encrypted_file'], 'rb') as f:
@@ -120,23 +137,23 @@ class Coding:
         except OSError as err:
             logging.warning(
                 f"{err} ошибка при чтении из файла {self.settings['encrypted_file']}")
-
         try:
             with open(self.settings['iv_path'], "rb") as f:
                 iv = f.read()
         except OSError as err:
             logging.warning(
                 f"{err} ошибка при чтении из файла {self.settings['iv_path']}")
-
         cipher = Cipher(algorithms.TripleDES(symmetric_key), modes.CBC(iv))
         decryptor = cipher.decryptor()
         dc_text = decryptor.update(en_text) + decryptor.finalize()
         unpadder = sym_padding.ANSIX923(128).unpadder()
         unpadded_dc_text = unpadder.update(dc_text) + unpadder.finalize()
-
         try:
             with open(self.settings['decrypted_file'], 'wb') as f:
                 f.write(unpadded_dc_text)
         except OSError as err:
             logging.warning(
                 f"{err} ошибка при записи в файл {self.settings['decrypted_file']}")
+        else:
+            logging.info("Текст расшифрован")
+        return self.settings['decrypted_file']
--- main_w.py
+++ main_w.py
@@ -1,15 +1,19 @@
-from PyQt5.QtWidgets import QPushButton, QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QPixmap
-import sys
-from PyQt5 import QtWidgets
 import re
-from coding import Coding as cd
+import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton, QFileDialog)
+from coding import Encryption as en
 
 
 class Window(QMainWindow):
     def __init__(self) -> None:
-        super(Window, self).__init__()
+        """
+        Функция инициализации
 
+        """
+        super(Window, self).__init__()
         self.setWindowTitle('3DES')
         self.setFixedSize(600, 400)
         self.background = QLabel(self)
@@ -17,58 +21,76 @@ class Window(QMainWindow):
         self.background.setPixmap(QPixmap("icon.jpg").scaled(600, 400))
         self.info = QLabel(self)
         self.info.setText("Выберите размер ключа")
-        self.info.setGeometry(225, 20, 200, 50)
+        self.info.setGeometry(225, 20, 500, 50)
         self.message = QLabel(self)
         self.message.setGeometry(225, 250, 200, 50)
-
-        self.botton_keys = QPushButton('Сгенерировать ключи', self)
-        self.botton_keys.setGeometry(200, 100, 200, 50)
-        self.botton_keys.clicked.connect(self.cleak_key)
-        self.botton_keys.hide()
-
+        self.button_keys = QPushButton('Сгенерировать ключи', self)
+        self.button_keys.setGeometry(200, 100, 200, 50)
+        self.button_keys.clicked.connect(self.cleak_key)
+        self.button_keys.hide()
         self.key_size = QtWidgets.QComboBox(self)
         self.key_size.addItems(["64 бит", "128 бит", "192 бит"])
         self.key_size.setGeometry(200, 50, 200, 50)
         self.key_size.activated[str].connect(self.on_activated)
-
-        self.botton_e = QPushButton('Зашифровать текст', self)
-        self.botton_e.setGeometry(200, 150, 200, 50)
-        self.botton_e.clicked.connect(self.cleak_e)
-        self.botton_e.hide()
-
-        self.botton_d = QPushButton('Дешифровать текст', self)
-        self.botton_d.setGeometry(200, 200, 200, 50)
-        self.botton_d.clicked.connect(self.cleak_d)
-        self.botton_d.hide()
-
+        self.button_e = QPushButton('Зашифровать текст', self)
+        self.button_e.setGeometry(200, 150, 200, 50)
+        self.button_e.clicked.connect(self.cleak_encryption)
+        self.button_e.hide()
+        self.button_d = QPushButton('Дешифровать текст', self)
+        self.button_d.setGeometry(200, 200, 200, 50)
+        self.button_d.clicked.connect(self.cleak_decryption)
+        self.button_d.hide()
         self.show()
 
     def on_activated(self, text: str) -> None:
+        """
+        Функция присвоения размера ключа
+
+        """
         self.size = int(re.findall('(\d+)', text)[0])
         self.info.setText("Сгенирируйте ключи")
-        self.botton_keys.show()
+        self.button_keys.show()
 
     def hidden(self) -> None:
+        """
+        Функция показа кнопок для шифрования и дешифрования после генерации ключей
 
-        self.botton_d.show()
-        self.botton_e.show()
+        """
+        self.button_d.show()
+        self.button_e.show()
 
     def cleak_key(self) -> None:
-        self.key = cd(self.size)
+        """
+        Функция генерации ключей
+
+        """
+        way = str(QFileDialog.getExistingDirectory(caption='Выбор директории'))
+        self.key = en(self.size, way)
         self.key.generation_key()
         self.info.setText("Ключи сгенерированы")
         self.message.setText("Зашифруйте текст")
         self.hidden()
 
-    def cleak_e(self) -> None:
-        self.key.encryption()
+    def cleak_encryption(self) -> None:
+        """
+        Функция шифрования
+
+        """
+        way_e = str(QFileDialog.getOpenFileName(caption='Выберите файл для шифрования', filter='*.txt'))
+        way_e = way_e.split('\'')[1]
+        self.key.encryption(way_e)
         self.info.setText("Текст зашифрован")
         self.message.setText("Расшифруйте текст")
 
-    def cleak_d(self) -> None:
-        self.key.decryption()
+    def cleak_decryption(self) -> None:
+        """
+        Функция дешифорвания
+
+        """
+        way = self.key.decryption()
         self.info.setText("Текст расшифрован")
-        self.message.setText("Готово!")
+        self.message.setGeometry(120,250,600,30)
+        self.message.setText(way)
 
 
 def application() -> None:
